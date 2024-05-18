f6f695d68f8457d05968da07b59232efb31b46c8
marym-ahmed <82773504+marym-ahmed@users.noreply.github.com>
2024-05-18 04:36:09 +0300

    second commit

---
+++ app/Services/UpdateDeviceToken.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\services;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UpdateDeviceToken
+{
+    public function updateDeviceToken($request)
+    {
+        Auth::user()->device_token =  $request->token;
+        Auth::user()->save();
+
+        return response()->json(['Token successfully stored.']);
+    }
+}
